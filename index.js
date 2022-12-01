6eabc5649d11d7d6f3c2cd9f15b50f6bf311b135
holmancardozomt <holman.cardozo278@casur.gov.co>
2022-12-01 12:56:51 -0500

    Nodemon y Router

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from 'express'
+const router = express.Router()
 
 const app = express()
 
-app.get('/',  (req, res) => {
-  res.send('🤖🛸Primer Mensaje de la clase 🤑🤖 Deployment  en Vercel Desarrollo de Aplicaciones Web🛸🤖')
+app.use(router)
+
+router.get('/',function (req, res) {
+  res.send('Hola desde la raiz😁')
 })
 
-app.listen(8000, () => {
-    console.log("El Servidor esta corriendo en http://localhost:8000")
+router.get('/user', (req, res) => {
+  res.send('Obteniendo Informacion😀')
+})
+
+router.post('/login', (req, res) => {
+  res.send('Logiandome desde Login😎')
+})
+
+router.put ('/update', (req, res) => {
+  res.send('Actualizando los usuarios🤗')
+})
+
+router.delete('/delete', (req, res) => {
+  res.send('Eliminando un usuario😮')
+})
+
+app.get('/mensaje',  (req, res) => {
+  res.send('🤖🛸Primer Mensaje de la clase 🤑🚀🤖 Deployment  en Vercel Desarrollo de Aplicaciones Web🛸🤖')
+})
+
+const puerto = 5000;
+app.listen(puerto, () => {
+    console.log(`El Servidor esta corriendo en http://localhost:${puerto}`)
 })
